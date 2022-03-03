276360f79795d8666d40affad7d9453df4dcb7c3
kessyads <kesia.adsantos@gmail.com>
2022-03-03 20:10:46 -0300

    second_update

---
+++ script.js
@@ -0,0 +1,7 @@
+function logInn()  {
+  }
+  
+const email = document.querySelector('email');
+const password = document.querySelector('password');
+email.addEventListener('Entrar', logInn);
+password.addEventListener('Entrar', logInn)
