51ec0ea8c0ba0ea5afa93774b1d07dd74c6453f4
kessyads <kesia.adsantos@gmail.com>
2022-03-03 21:47:53 -0300

    third_update

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-function logInn()  {
+function onSubmit(event){
+    event.preventDefault()
+    const emailInput = event.target.elements["email"]
+    const passwordInput = event.target.elements["password"]
+    const isEmailValid = validateEmail(emailInput.value)
+    const isPasswordValid = checkPassword(passwordInput.value)
+    if(!isEmailValid){
+        emailInput.classList.add("error")
+    }
+    else{
+        emailInput.classList.remove("error")
+    }
+    if(!isPasswordValid){
+        passwordInput.classList.add("error")
+    }
+        else{
+            passwordInput.classList.remove("error")
+        }
+    }
+
+const form = document.querySelector("form")
+form.addEventListener('submit', onSubmit)
+
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
   }
-  
-const email = document.querySelector('email');
-const password = document.querySelector('password');
-email.addEventListener('Entrar', logInn);
-password.addEventListener('Entrar', logInn)
+
+  function checkPassword(str) {
+    if(str.length < 5 || str.length >10){
+        return false
+    }
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(str);
+}
+
